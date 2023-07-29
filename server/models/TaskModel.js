2439c146d64bec6302d2bc9be24fb57f5ddbeb90
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-07-29 13:01:45 -0500

    install required node modules and set up SQL database

---
+++ server/models/TaskModel.js
@@ -0,0 +1,18 @@
+const {Pool} = require('pg');
+
+const myURI = 'postgres://xrriwzxh:rnKutmCqLTfxlpIfjqlxNUWAGioa_ZoU@mahmud.db.elephantsql.com/xrriwzxh';
+
+const pool = new Pool({
+  connectionString: myURI,
+});
+
+pool.on('connect', () => {
+  console.log('Connected to PostgreSQL database');
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('Executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
