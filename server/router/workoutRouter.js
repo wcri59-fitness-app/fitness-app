2c937208bb91227979ab3a19bf4475568029379a
liamroh <liamrohx@gmail.com>
2023-07-31 15:47:21 -0700

    added additional routes

--- server/router/workoutRouter.js
+++ server/router/workoutRouter.js
@@ -5,7 +5,7 @@ const db = require ('../models/TaskModel');
 
 // currently have 5 tables
 // Table 1. (Users table) Columns => user_id, username, password
-// Table 2. (Workouts table) Columns => workout_id, user_id, workout_date, workout_name 
+// Table 2. (Workouts table) Columns => workout_id, user_id, workout_date, workout_name
 // Table 3. (SetAndReps table) Columns => SAR_id, sets, reps
 // Table 4. (Exercise table) Columns => exercise_id, exercise
 // Table 5. (WorkoutExercise Table) Columns => workout_id, exercise_id, SAR_id, weight_used
@@ -17,9 +17,27 @@ router.get("/", async (req, res, next) => {
     // testing to see if the db connects
     // const test = await db.query('SELECT * FROM Workouts');
     try {
-        const {user_id} = req.params;
-        const exercises = await db.query(`SELECT user_id, Workouts.workout_id FROM Workouts LEFT JOIN WorkoutExercise ON Workouts.workout_id = WorkoutExercise.workout_id WHERE Workouts.workout_id = ${user_id}`);
-        res.status(200).json(exercises.rows);
+        // const {user_id} = req.params;
+        user_id = 1;
+        const exercises = await db.query(`
+            SELECT 
+            we.workout_id,
+            we.exercise_id,
+            we.sar_id,
+            we.weight_used,
+            w.workout_date,
+            u.user_id,
+            u.username,
+            e.exercise,
+            ed.reps,
+            ed.sets
+            FROM WorkoutExercise AS we
+            JOIN Workouts AS w ON we.workout_id = w.workout_id
+            JOIN Users AS u ON w.user_id = u.user_id
+            JOIN Exercise AS e ON we.exercise_id = e.exercise_id
+            JOIN setandreps AS ed ON we.sar_id = ed.sar_id; WHERE w.user_id = ${user_id}`);
+        // const exercises = await db.query('SELECT * FROM WorkoutExercise');
+        res.status(200).json(exercises.rows); // .rows
     }
     catch (error) {
         return next({
@@ -37,22 +55,31 @@ router.get("/", async (req, res, next) => {
 // router.post()
 router.post("/add", async (req, res, next) => {
     try {
+        console.log(req.body);
+        console.log(req.params);
+        const {userId} = req.body;
         const currentDate = new Date().toISOString().split('T')[0];
         const workoutQuery = "INSERT INTO Workouts (user_id, workout_date) VALUES ($1, $2)";
         const workoutValues = [userId, currentDate];
         const workoutResult = await db.query(workoutQuery, workoutValues);
+        console.log(workoutResult);
 
         const {sets, reps} = req.body;
-        const SARQuery = "INSERT INTO SetAndReps (Sets, reps) values ($1, $2)";
+        const SARQuery = "INSERT INTO SetAndReps (sets, reps) values ($1, $2)";
         const SARResult = await db.query(SARQuery, [sets, reps]);
+        console.log(SARResult);
 
         const {exercise} = req.body;
-        const ExerciseQuery = `INSERT INTO Exercises (exercise) values (${exercise})`;
-        const ExerciseResult = await db.query(ExerciseQuery);
+        const ExerciseQuery = `INSERT INTO Exercise (exercise) values ($1)`;
+        const ExerciseResult = await db.query(ExerciseQuery, [exercise]);
+        console.log(ExerciseResult);
 
-        const { workout_id, exercise_id, SAR_id, weight_used } = req.body;
-        const workoutExerciseQuery = `INSERT INTO WorkoutExercise (workout_id, exercise_id, SAR_id, weight_used) values (${workout_id, exercise_id, SAR_id, weight_used})`
-        const workoutExerciseResult = await db.query(workoutExerciseQuery)
+        const { workout_id, exercise_id, sar_id, weight_used } = req.body;
+        const workoutExerciseQuery = `INSERT INTO WorkoutExercise (workout_id, exercise_id, sar_id, weight_used) VALUES ($1, $2, $3, $4)`;
+        const workoutExerciseResult = await db.query(workoutExerciseQuery, [workout_id, exercise_id, sar_id, weight_used]);
+        console.log(workoutExerciseResult);
+        
+        res.status(200).json(workoutExerciseResult.rows);
         
        return next();
     } catch (error) {
@@ -62,5 +89,41 @@ router.post("/add", async (req, res, next) => {
         })
     }
 })
+
+router.delete("/delete", async (req, res, next) => {
+    try {
+        const {workout_id} = req.params;
+        const deleteQueryWorkouts = `DELETE FROM Workouts WHERE workout_id = ${workout_id}`; // removing workout from the workout table
+        const deleteResultWorkouts = await db.query(deleteQueryWorkouts);
+        const deleteQueryWorkoutExercise = `DELETE FROM WorkoutExercise WHERE workout_id = ${workout_id}`; // removing workout from the workout table
+        const deleteResultWorkoutExercise = await db.query(deleteQueryWorkoutExercise);
+        const ret = deleteResultWorkouts.rows.concat(deleteResultWorkoutExercise.rows);
+        res.status(200).json(ret);
+        return next();
+    } catch (error) {
+        return next({
+            log: "error with workoutRouter",
+            message: {err:"Query Failed"}
+        })
+    }
+})
+
+// router.patch("/update", async (req, res, next) => {
+//     try {
+//         const {workout_id} = req.params;
+//         const deleteQueryWorkouts = `DELETE FROM Workouts WHERE workout_id = ${workout_id}`; // removing workout from the workout table
+//         const deleteResultWorkouts = await db.query(deleteQueryWorkouts);
+//         const deleteQueryWorkoutExercise = `DELETE FROM WorkoutExercise WHERE workout_id = ${workout_id}`; // removing workout from the workout table
+//         const deleteResultWorkoutExercise = await db.query(deleteQueryWorkoutExercise);
+//         const ret = deleteResultWorkouts.rows.concat(deleteResultWorkoutExercise.rows);
+//         res.status(200).json(ret);
+//         return next();
+//     } catch (error) {
+//         return next({
+//             log: "error with workoutRouter",
+//             message: {err:"Query Failed"}
+//         })
+//     }
+// })
 
 module.exports = router; 
