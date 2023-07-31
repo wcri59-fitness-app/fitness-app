753b5ad2c254685e5f7501e05e78fc8e696d8230
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-07-31 17:21:21 -0500

    add colum name to workouts

--- server/router/workoutRouter.js
+++ server/router/workoutRouter.js
@@ -5,9 +5,9 @@ const db = require ('../models/TaskModel');
 
 // currently have 5 tables
 // Table 1. (Users table) Columns => user_id, username, password
-// Table 2. (Workouts table) Columns => workout_id, user_id, workout_date
+// Table 2. (Workouts table) Columns => workout_id, user_id, workout_date, workout_name 
 // Table 3. (SetAndReps table) Columns => SAR_id, sets, reps
-// Table 4. (Exercises table) Columns => exercise_id, exercise
+// Table 4. (Exercise table) Columns => exercise_id, exercise
 // Table 5. (WorkoutExercise Table) Columns => workout_id, exercise_id, SAR_id, weight_used
 // The workout we create on the front end will have a unique id so we can connect multiple exercises to it. each exercise can have a unique amount of set and rep.
 // users are connected to the workouts on th eworkout table.
--- server/server.js
+++ server/server.js
@@ -34,13 +34,13 @@ app.use('/workout', workoutRouter);
 //end point for signups
 // do we need routers for signup and login? 
 app.post('/signup', userController.createUser, (req, res) => {
-res.sendStatus(200)
+res.redirect('login').status(200)
 })
 
 //end point for logins
 // logging in and out? how to make persisting session?
 app.get('/login', userController.verifyUser, cookieController.setCookie, (req, res) => {
-res.sendStatus(200)
+res.redirect('/').status(200)
 })
 
 
